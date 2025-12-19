e2ff0d92625be7d1a4fbad8201c3c0b0ba863198
Brouq <plialq.077@gmail.com>
2025-12-19 17:52:10 +0300

    update

--- Fasih/main/views.py
+++ Fasih/main/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 
 def home(request):
     return render(request, "main/home.html")
 
+def home(request):
+    if request.method == 'POST':
+        # نقدر نستخدم البيانات لاحقًا
+        first_name = request.POST.get('first_name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        messages.success(request, "تم إرسال رسالتك بنجاح، سنعود لك قريبًا")
+        return redirect('home')
+
+    return render(request, 'main/home.html')
+
+
+
+def about(request):
+    return render(request, "main/about.html")
